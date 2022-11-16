da3fb7e0bfef7416f8c51f0428429a9f02512fae
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-16 15:25:02 +0600

    Box Design
    Single Box CSS Code Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Box from './Components/Box/Box';
 
 function App() {
   return (
     <div className="App">
-
+      <Box value="X" onClick={null}></Box>
     </div>
   );
 }
