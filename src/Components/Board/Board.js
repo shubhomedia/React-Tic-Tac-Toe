768f0c484369558a13a2cdd28736f650133a201b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-17 18:40:58 +0600

    Create Board

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Box from '../Box/Box';
+import './Board.css';
+
+
+const Board = ({ board, onClick }) => {
+    return (
+        <div className='board'>
+            {
+                board.map((value, idx) => {
+                    return <Box value={value} onClick={() => value === null && onClick(idx)} />;
+                })
+            }
+        </div>
+    );
+};
+
+export default Board;
