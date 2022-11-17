2f6b703111f2b2fe1aa8c0691df93b147d129fa1
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-17 18:40:51 +0600

    Add New Footer About Section

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+const About = () => {
+    return (
+        <div className='about'>
+            <h1>React Tic-Tac-Toe</h1>
+            <h2>Design And Developed By <a href="https://shubho.info">Shubho.Info</a></h2>
+        </div >
+    );
+};
+
+export default About;
